9aed193ab06e74ed9a19f93e8ba01a8f4fe9599c
Vishruth <notvishruth@gmail.com>
2026-05-25 21:52:38 +0530

    first commit

---
+++ src/main/java/com/example/snake/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.snake;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(SnakeGame.class, args);
+    }
+}
---
+++ src/main/java/com/example/snake/SnakeGame.java
@@ -0,0 +1,15 @@
+package com.example.snake;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SnakeGame extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+}
