17fb9efb6963cc921be115c6aa1122593061a526
Vishruth <notvishruth@gmail.com>
2026-05-27 22:52:42 +0530

    Setting up the stage...

--- src/main/java/com/example/snake/SnakeGame.java
+++ src/main/java/com/example/snake/SnakeGame.java
@@ -2,14 +2,23 @@ package com.example.snake;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SnakeGame extends Application {
+    private static int WIDTH = 600;
+    private static int HEIGHT = 600;
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println("Hello World");
+        Group group = new Group();
+        Scene scene = new Scene(group, WIDTH, HEIGHT);
+        stage.setScene(scene);
+        stage.show();
+        Canvas canvas = new Canvas();
     }
 }
